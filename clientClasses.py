23a80770d462344972bd418f03634705a7619bcd
logroa <72048746+logroa@users.noreply.github.com>
2021-01-19 01:41:37 -0500

    Graph and Error Checking
    Visualizing the DataFrame comparing an entity's daily relevance and popular sentiment;  Checking and handling the imposed Twitter Rate Limit on tweet data

--- clientClasses.py
+++ clientClasses.py
@@ -14,19 +14,10 @@ import pandas as pd
 import re
 import matplotlib.pyplot as plt
 
-consumer_key = "xxxxx"
-consumer_secret = "xxxxxxx"
-access_token = "xxxxxxx"
-access_token_secret = "xxxxxxxx"
-
-# auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-# auth.set_access_token(access_token, access_token_secret)
-# api = tweepy.API(auth)
-
-# popularity metric that measures absolute value of relevance
-# number of tweets + number of likes + number of retweets*3 + number of replies
-# directional for love or hate
-
+consumer_key = "AUds4eS1BHAq2UcjeaBbmQi0K"
+consumer_secret = "iVWKUBzAclrz1ow3XBP5ReMwAX6WUBO9NaXlCYEeiHGj5UVN7a"
+access_token = "1319171958286749697-n8l6vveS4glZHOQUHxnH6ozI6ek2eI"
+access_token_secret = "X9WKg1NTCmc96R9KscIpJrtQyLcHRMLF1YGuwAgT0MPy2"
 
 class TwitterClient():
     def __init__(self, twitter_user=None):
@@ -58,12 +49,25 @@ class TwitterClient():
 
     def keywords_search(self, keywords, num_tweets, startDate, endDate):
         tweets = []
-        for tweet in Cursor(self.twitter_client.search, q=keywords, until=endDate, lang="en").items(num_tweets):
-            if tweet.retweet_count > 0:
-                if tweet not in tweets:
-                    tweets.append(tweet)
-            else:
-                tweets.append(tweet)
+
+        data = Cursor(self.twitter_client.search, q=keywords, until=endDate, lang="en").items(num_tweets)
+
+        while True:
+          try:
+              tweet = data.next()
+
+              if tweet.retweet_count > 0:
+                  if tweet not in tweets:
+                      tweets.append(tweet)
+              else:
+                  tweets.append(tweet)
+
+          except tweepy.TweepError: #exception for twitter rate limits
+            print("Twitter's free API limit rate has been reached.  More data can be requested in fifteen minutes.  Here is what we were able to pull: ")
+            break
+          except Exception as e:
+            break
+
         return tweets
 
 class TwitterAuthenticator():
@@ -98,7 +102,7 @@ class TwitterListener(StreamListener):
         return True
 
     def on_error(self, status):
-        if status == 420:
+        if status == 429:
             # check for twitter rates limit to prevent banning
             return False
         print(status)
@@ -122,9 +126,7 @@ class TweetAnalyzer():
 
 
     def actual_score(self, sentiment, likes, retweets):
-
-        return 6*sentiment + sentiment*likes + sentiment*retweets*3
-
+        return sentiment * (6 + 3*retweets + likes)
 
     def tweets_to_dataframe(self, tweets):
         df = pd.DataFrame(data=[tweet.text for tweet in tweets], columns=['tweets'])
@@ -145,29 +147,21 @@ class TweetAnalyzer():
     def date_grouper(self, df):
         days = [date.split(" ")[0] for date in df['when'].values]
         df['day'] = days
-        tweetsGrouped = df[['day', 'pop', 'score']].groupby('day')['score'].agg(np.sum)
-        # tweetsGrouped['daily pop'] = tweetsGrouped['pop'].agg(np.sum)
-        # tweetsGrouped['daily score'] = tweetsGrouped['score'].agg(np.sum)
-        return tweetsGrouped 
+        tweetsGrouped = df[['day', 'pop', 'score']].groupby('day')['pop'].agg(np.sum)
+        tweetsGrouped1 = df[['day', 'pop', 'score']].groupby('day')['score'].agg(np.sum)
+
+        df2 = pd.DataFrame({'Relevance' : tweetsGrouped, 'Popularity' : tweetsGrouped1}).reset_index()
+        return df2 
 
 if __name__ == "__main__":
     
-    hash_tag_list = ["andy dalton", "red rifle"]
-    """
-    fetched_tweet_filename = "tweets.json"
+    hash_tag_list = ["andy dalton"]
 
-    twit_cli = TwitterClient("colincowherd")
-    print(twit_cli.get_tweets(1))
-
-    twit_streamer = TwitterStreamer()
-    twit_streamer.stream_tweets(fetched_tweet_filename, hash_tag_list)
-    """
     twitter_client = TwitterClient()
     tweet_analyzer = TweetAnalyzer()
     api = twitter_client.get_twitter_client_api()
 
-    # tweets = api.user_timeline(screen_name="colincowherd", count=200)
-    tweets = twitter_client.keywords_search(hash_tag_list, 1000, dt.date.today()-dt.timedelta(days=30), dt.date.today())
+    tweets = twitter_client.keywords_search(hash_tag_list, 10000, dt.date.today()-dt.timedelta(days=30), dt.date.today())
 
     df = tweet_analyzer.tweets_to_dataframe(tweets)
 
@@ -177,15 +171,12 @@ if __name__ == "__main__":
 
     df['score'] = tweet_analyzer.actual_score(df['sentiment'], df['likes'], df['retweets'])
 
-    # print(tweets[2].retweet_count)
-    # print(dir(tweets[0]))
-
     out = tweet_analyzer.date_grouper(df)
 
-    print(out.head(20))
-    # print(np.mean(df['retweets']))
+    print(out)
 
     # Time Series
-    # time_likes = pd.Series(data=df['likes'].values, index=df['when'])
+    # time_likes = pd.Series(data=out['Relevance'])
     # time_likes.plot(figsize=(16, 4), color='r')
     # plt.show()
+    out.plot(x='day', y=['Relevance', 'Popularity'], grid=True)
